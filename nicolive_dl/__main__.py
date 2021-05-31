afc479a65b2c8ce977349ce5ecd8e2042ae84162
kairi003 <kairi.satellite@gmail.com>
2021-05-31 17:57:09 +0900

    Update for pac

--- nicolive_dl/__main__.py
+++ nicolive_dl/__main__.py
@@ -6,12 +6,15 @@ from getpass import getpass
 
 from . import NicoLiveDl
 
-async def main():
+async def _main():
     nldl = NicoLiveDl()
     nldl.login(input('Account: '), getpass('Password: '))
     lvid = input('Live Id: ')
     print(await nldl.get_info(lvid))
     await nldl.download(lvid)
 
+def main():
+    asyncio.run(_main())
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
