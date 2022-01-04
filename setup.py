1faaed2c5e864ce739353dcd9ee0ea3296f34b16
kairi003 <kairi.satellite@gmail.com>
2022-01-05 03:31:42 +0900

    Fix setup

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup
+from nicolive_dl import __version__
 
-setup()
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setup(
+    name="nicolive_dl",
+    version=__version__,
+    license="MIT",
+    description="ニコ生タイムシフトダウンローダー",
+    author="kairi",
+    url="https://github.com/kairi003/nicolive-dl",
+    packages=['nicolive_dl'],
+    install_requires=_requires_from_file('requirements.txt'),
+    entry_points={
+        "console_scripts": ["nicolive_dl = nicolive_dl.__main__:main"]
+    }
+)
