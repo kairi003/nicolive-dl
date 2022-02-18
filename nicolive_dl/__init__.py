d7370d7a19dfdb08d45b51d732e27b57edcb78ee
kairi003 <kairi.satellite@gmail.com>
2022-02-18 15:53:06 +0900

    Fix shebang and more

--- nicolive_dl/__init__.py
+++ nicolive_dl/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from .nicolive_dl import NicoLiveDL, NicoLiveInfo
@@ -6,7 +6,7 @@ from .nicolive_ws import NicoLiveWS, WebSocketApp
 from .exceptions import *
 
 __copyright__    = 'Copyright (C) 2020 kairi'
-__version__      = '0.1.3'
+__version__      = '0.1.4'
 __license__      = 'MIT'
 __author__       = 'kairi'
 __author_email__ = 'kairi.satellite@gmail.com'
--- nicolive_dl/__main__.py
+++ nicolive_dl/__main__.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import asyncio
 from getpass import getpass
+import argparse
 
 from . import NicoLiveDL
 
-async def _main():
+async def _main(username, password, live_id):
     nldl = NicoLiveDL()
-    nldl.login(input('Account: '), getpass('Password: '))
-    lvid = input('Live Id: ')
-    await nldl.download(lvid)
+    nldl.login(username, password)
+    await nldl.download(live_id)
 
 def main():
-    asyncio.run(_main())
+    username = input('Account: ')
+    password = getpass('Password: ')
+    live_id = input('Live Id: ')
+    asyncio.run(_main(username, password, live_id))
 
 if __name__ == '__main__':
     main()
