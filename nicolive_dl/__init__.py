cad34356e04d6418e72471832b0b50a83df64731
zhyu <angellwings@gmail.com>
2023-05-22 13:17:07 +0900

    use the LIVE_URL_PREFIX constant in main

--- nicolive_dl/__init__.py
+++ nicolive_dl/__init__.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .nicolive_dl import NicoLiveDL, NicoLiveInfo
-from .nicolive_ws import NicoLiveWS, WebSocketApp
 from .exceptions import *
+from .nicolive_dl import LIVE_URL_PREFIX, NicoLiveDL, NicoLiveInfo
+from .nicolive_ws import NicoLiveWS, WebSocketApp
 
-__copyright__    = 'Copyright (C) 2020 kairi'
-__version__      = '0.1.4'
-__license__      = 'MIT'
-__author__       = 'kairi'
-__author_email__ = 'kairi.satellite@gmail.com'
-__url__          = 'https://github.com/kairi003/nicolive-dl'
+__copyright__ = "Copyright (C) 2020 kairi"
+__version__ = "0.1.4"
+__license__ = "MIT"
+__author__ = "kairi"
+__author_email__ = "kairi.satellite@gmail.com"
+__url__ = "https://github.com/kairi003/nicolive-dl"
 
-__all__ = ['NicoLiveDL', 'NicoLiveWS', 'WebSocketApp', 'NicoLiveInfo', 'LoginError', 'SelectException']
+__all__ = [
+    "NicoLiveDL",
+    "NicoLiveWS",
+    "WebSocketApp",
+    "NicoLiveInfo",
+    "LoginError",
+    "SelectException",
+    "LIVE_URL_PREFIX",
+]
